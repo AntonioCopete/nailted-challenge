5fcddc706d34d6598675678738f0b2026992f57c
AntonioCopete <antcopjim@gmail.com>
2022-08-29 14:30:49 +0200

    feat: find employees with filter, sort and pagination

--- src/controllers/employeeController.ts
+++ src/controllers/employeeController.ts
@@ -1,13 +1,24 @@
-import { RequestHandler } from "express";
+import { Request, RequestHandler, Response } from "express";
 import Employee from "../models/Employee";
+import { Query } from "express-serve-static-core";
 
-export const getEmployees: RequestHandler = async (req, res) => {
-    Employee.find();
+interface IRequest<T extends Query> extends Request {
+    query: T;
+}
 
-    res.status(200).json({ msg: "DONE" });
+interface IQueryParams {
+    [index: string]: string;
+}
+
+export const getEmployees = async (req: IRequest<IQueryParams>, res: Response) => {
+    const { page, sort, filter }: IQueryParams = req.query;
+
+    const employees = Employee.find(page, sort, filter);
+
+    return res.status(200).json({ employees });
 };
 
-export const createEmployee: RequestHandler = async (req, res) => {
+export const createEmployee = async (req: Request, res: Response) => {
     const { name, surname, address, phone, email, birthdate } = req.body;
     console.log(name);
 
--- src/models/Employee.ts
+++ src/models/Employee.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 interface IEmployee {
-    id?: number;
+    id?: string;
     name: string;
     surname: string;
     address: string;
@@ -13,7 +13,8 @@ const Employee = {
     create: function ({ name, surname, address, phone, email, birthdate }: IEmployee) {
         // Getting the last employee id to create an employee with the next id
         const file = fs.readFileSync("./src/db/employees.txt");
-        const lastId = file.toString().split("\n")[file.toString().split("\n").length - 1].split(",")[0];
+        const textLines = file.toString().split("\n");
+        const lastId = textLines[textLines.length - 1].split(",")[0];
         const newId = parseInt(lastId) + 1;
 
         // Sorting and formatting employee data before creating a new employee
@@ -22,11 +23,33 @@ const Employee = {
 
         fs.appendFileSync("./src/db/employees.txt", "\n" + stringEmployee);
     },
-    find: function () {
+    find: function (page: string = "1", sort: string, filter: string) {
         const file = fs.readFileSync("./src/db/employees.txt");
-        const dbEmployees = file.toString().split("\n");
 
-        console.log(dbEmployees);
+        let employees: IEmployee[] = file
+            .toString()
+            .split("\n")
+            .map((textLine) => {
+                const employee = textLine.split(",");
+
+                const [id, name, surname, address, phone, email, birthdate] = employee;
+
+                return { id, name, surname, address, phone, email, birthdate };
+            });
+
+        if (filter) {
+            employees = employees.filter((employee: IEmployee) => employee.email.includes(filter));
+        }
+
+        if (sort === "name" || sort === "surname") {
+            console.log(sort);
+
+            employees.sort((a: any, b: any) => a[sort].localeCompare(b[sort]));
+        }
+
+        employees = employees.slice(parseInt(page) * 5 - 5, parseInt(page) * 5);
+
+        return employees;
     },
 };
 
